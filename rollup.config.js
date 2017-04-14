90950f8142a1b373fb3f960c3aa0243f45185d82
Yuriy Husnay <husnaj23@gmail.com>
2017-04-14 18:44:18 +0300

    fix browser global name

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   entry: 'src/index.js',
   format: 'umd',
-  moduleName: 'autobindr',
+  moduleName: 'autobind',
   plugins: [babel()],
   dest: 'dist/autobindr.js'
 };
